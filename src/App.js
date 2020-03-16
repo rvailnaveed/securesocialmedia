0cd59423652476e123af7c1403b9dd0392cf55df
Rvail Naveed <naveedm@tcd.ie>
2020-03-16 16:09:14 +0000

    setup navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
+import Home from "./components/Home"
+import Error from "./components/Error"
+import Navbar from "./components/Navbar"
 
 function App() {
-  return null;
+  return (
+    <main>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route component={Error} />
+      </Switch>
+    </main>
+  )
 }
 
 export default App;

75538f15de4e8c6b661d1b420a4edb1e172d4928
rvailnaveed <37183938+rvailnaveed@users.noreply.github.com>
2020-04-11 17:15:45 +0100

    remove temp ref

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Copy from "./copy";
 
 ReactDOM.render(
     <App />,
-    //<Copy/>,
     document.getElementById('root')
 );
 
